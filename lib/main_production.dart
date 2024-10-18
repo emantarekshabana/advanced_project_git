9a9f9e4b3c4bceb58196ce2f9bb3105a46ec2a78
emantarekshabana <emantarekshabana@gmail.com>
2024-10-18 14:52:42 +0300

    - added Development & Productions flavors for Android. - changed Android app name for each flavor. - added launch.json for flavors config. - added two main files for each flavor.

---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+import 'package:advanced_project/core/di/dependency_injection.dart';
+import 'package:advanced_project/core/routing/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'doc_app.dart';
+
+void main() async{
+  setupGetIt();
+    // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+  runApp(DocApp(appRouter : AppRouter(),)); //act as client app
+}
